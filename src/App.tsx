73e90b4d007d23ad44d8fa7f0cb14fddf93a059b
KatSchultz <ks.katieschultz@gmail.com>
2022-08-13 19:20:41 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Events from "./pages/Events";
+import Photos from "./pages/Photos";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Marge's Bar and Grill</h1>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/menu"}>Menu</Link>
+        <Link to={"/contact"}>Contact Us</Link>
+        <Link to={"/events"}>Events</Link>
+        <Link to={"/photos"}>Photos</Link>
       </header>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/events" element={<Events />}></Route>
+        <Route path="/photos" element={<Photos />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/data/MenuItems.ts
@@ -0,0 +1,11 @@
+import { MenuItem } from "../types";
+
+export const appetizers: MenuItem[] = [
+  { name: "French Fries", description: "", price: 0.0 },
+  { name: "Seasoned Fries", description: "", price: 0.0 },
+  { name: "Onion Rings", description: "", price: 0.0 },
+  { name: "Mozarella Sticks", description: "", price: 0.0 },
+  { name: "Mini Tacos", description: "", price: 0.0 },
+  { name: "Red Wings", description: "", price: 0.0 },
+  { name: "Wing Dings", description: "", price: 0.0 },
+];
